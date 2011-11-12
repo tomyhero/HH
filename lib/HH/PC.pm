48352c6eb617b38955d77fef2148de40bcc19837
Tomohiro Teranishi <tomohiro.teranishi@gmail.com>
2011-11-12 21:27:14 +0900

    hehe

--- lib/HH/PC.pm
+++ lib/HH/PC.pm
@@ -2,8 +2,4 @@ package HH::PC;
 use Ze::Class;
 extends 'Ze::WAF';
 
-__PACKAGE__->context_class('HH::PC::Context');
-__PACKAGE__->dispatcher_class('HH::PC::Dispatcher');
-__PACKAGE__->view_class('HH::PC::View');
-
 EOC;
