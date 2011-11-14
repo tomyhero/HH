06076c1e9dbda7f53bf976a2566def66ef101cff
Tomohiro Teranishi <tomohiro.teranishi@gmail.com>
2011-11-14 23:10:08 +0900

    errrrrrrrorrrr

--- lib/HH/Validator/Error.pm
+++ lib/HH/Validator/Error.pm
@@ -1,9 +1,6 @@
 package HH::Validator::Error;
 use Ze::Class;
 extends 'Aplon::Error';
-
-has 'error_message' => ( is => 'rw' , default => sub { {} } );
-
-
+with 'Aplon::Error::Role::LazyWay';
 
 EOC;
