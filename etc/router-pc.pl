9d9820aec7dabac5eba9c3fe620a95e9b4b7f7ce
Tomohiro Teranishi <tomohiro.teranishi@gmail.com>
2011-11-17 19:44:36 +0900

    router

--- etc/router-pc.pl
+++ etc/router-pc.pl
@@ -1,6 +1,6 @@
 return router {
     submapper('/', {controller => 'Root'})
-        ->connect('', {action => 'index' ,  page_cache => { expire => 60 } })
+        ->connect('', {action => 'index' ,  page_cache => { expire => 60 , query_keys => [qw/url/] } })
         ->connect('{url:https?://[-_.!~*\'()a-zA-Z0-9;/?:\@&=+\$,%#]+}', {action => 'detail', use_anticsrf => 1 , page_cache => { expire => 60 } }) ;
 
     submapper('/auth/', {controller => 'Auth'})
